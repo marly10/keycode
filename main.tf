c93b678d74aee305a07a66ed18f2fab986754ad9
marly10 <mckmarly@icloud.com>
2022-09-04 14:28:00 -0700

    send

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-data "aws_security_group" "ssh" {
-  id = "sg-0747026767ceec6b5"
-}
-
 # cloud-init config that installs the provisioning scripts
 data "local_file" "write_ssh_interval" {
   filename = "cloud-data"
@@ -31,7 +27,8 @@ resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = "aws_lajolla_public"
-  user_data = data.cloudinit_config.provision.rendered
+  #user_data = data.cloudinit_config.provision.rendered
+
   vpc_security_group_ids = [
     "sg-0747026767ceec6b5",
     "sg-06700b3cf3acc1772"
@@ -64,30 +61,35 @@ resource "aws_instance" "ec2" {
   }
 
   provisioner "file" {
-    source      = "/Users/rickymarly/.ssh/aws_lajolla"
-    destination = "/home/ubuntu/aws_lajolla"
+    source      = "/Users/rickymarly/.ssh/aws_lajolla_public.pem"
+    destination = "/home/ubuntu/aws_lajolla_public.pem"
   }
 
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
-    private_key = file("/Users/rickymarly/.ssh/aws_lajolla")
-    timeout = "30m"
+    private_key = file("/Users/rickymarly/.ssh/aws_lajolla_public.pem")
+    timeout = "4m"
   }
 
   provisioner "remote-exec" {
     inline = [
       "touch hello.txt",
       "echo hello world remote provisioner >> hello.txt",
-      "sudo apt update",
-      "sudo apt install ansible",
-      #sudo apt install ansible
+      #"sudo apt -y update",
+      #"sudo apt install -y ansible",
+      #"sudo apt install -y yamllint",
+      
       "chmod +x /home/ubuntu/var.sh",
-      "bash /home/ubuntu/var.sh",
-      "ansible-pull --accept-host-key -d /home/ubuntu/git --key-file=/home/ubuntu/aws_lajolla -C HEAD -U 'git@github.com:marly10/keycode.git' -i hosts",
+      "sudo bash /home/ubuntu/var.sh",
     ]
   }
+
+ /* provisioner "local-exec" {
+command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u centos --private-key ./Test-Key-1.pem -T 300 -i ${aws_instance.example.public_ip}, playbook.yml"
+}*/
+
   //ansible-pull --accept-host-key -d /home/ubuntu/git --key-file=/home/ubuntu/aws_lajolla -C HEAD -U 'git@github.com:marly10/keycode.git' -i hosts
 
   //command used to install splunk:
@@ -97,5 +99,12 @@ resource "aws_instance" "ec2" {
   //wget -O splunk-9.0.0.1-9e907cedecb1-linux-2.6-x86_64.rpm "https://download.splunk.com/products/splunk/releases/9.0.0.1/linux/splunk-9.0.0.1-9e907cedecb1-linux-2.6-x86_64.rpm"
   //sudo alien *.rpm
   //sudo dpkg -i package_name.deb or sudo apt install ./package_name.deb
+
+  /*
+  cat << 'EOF' > /home/ubuntu/.ssh/sshd_config
+ClientAliveInterval 120 
+ClientAliveCountMax 720 
+EOF
+  */
 }
 
--- var.sh
+++ var.sh
@@ -1,3 +1,10 @@
+echo "Sleeping for 30 Secoonds"
+sleep 30
+echo "Sleept for 30 Secoonds"
+
+sudo apt -y update
+sudo apt install -y ansible
+sudo apt install -y yamllint
 mkdir addingThings
 touch addingThings/nada.txt
 touch /home/ubuntu/.ssh/sshd_config 
@@ -8,4 +15,4 @@ ClientAliveCountMax 720
 EOF
 echo "this is my ansible-tf life" > addingThings/nada.txt
 echo "localhost ansible_connection=local"  > hosts
-
+#ansible-pull --accept-host-key -d /home/ubuntu/git --key-file=/home/ubuntu/aws_lajolla_public.pem -C HEAD -U 'git@github.com:marly10/keycode.git' -i hosts
