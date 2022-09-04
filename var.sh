9d3812f26cdc6f5ed0455ff381ffe4167b85a621
marly10 <mckmarly@icloud.com>
2022-09-04 15:44:34 -0700

    send

--- var.sh
+++ var.sh
@@ -3,7 +3,12 @@ sleep 30
 echo "Sleept for 30 Secoonds"
 
 sudo apt -y update
-sudo apt install -y ansible
+sudo apt -y upgrade
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+sudo apt -y install ansible
+
+
 sudo apt install -y yamllint
 mkdir addingThings
 touch addingThings/nada.txt
@@ -14,5 +19,12 @@ ClientAliveInterval 120
 ClientAliveCountMax 720 
 EOF
 echo "this is my ansible-tf life" > addingThings/nada.txt
-echo "localhost ansible_connection=local"  > hosts
+echo "[all]"  "localhost ansible_connection=local"  > hosts
 #ansible-pull --accept-host-key -d /home/ubuntu/git --key-file=/home/ubuntu/aws_lajolla_public.pem -C HEAD -U 'git@github.com:marly10/keycode.git' -i hosts
+#ansible 2.9.27
+
+echo "The AnsiblevVersion i want ansible 2.9.27" 
+ansible --version
+echo "Completed Provisioning" 
+ansible-pull -U https://github.com/marly10/keycode.git --key-file=/home/ubuntu/aws_lajolla_public.pem -C master file_creator.yml
+
